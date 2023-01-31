b044849a4af89c16585931894c2615761f788ddf
Himalayan <hdnpress@gmail.com>
2023-01-31 14:54:30 +0545

    Some refactor

--- resources/views/mechanics/index.blade.php
+++ resources/views/mechanics/index.blade.php
@@ -16,13 +16,13 @@
                             <thead
                                 class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th scope="col" class="px-6 py-2">
                                         ID
                                     </th>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th scope="col" class="px-6 py-2">
                                         Mechanic
                                     </th>
-                                    <th scope="col" class="px-6 py-3">
+                                    <th scope="col" class="px-6 py-2">
                                         Car Owner
                                     </th>
                                 </tr>
@@ -30,14 +30,14 @@
                             <tbody>
                                 @foreach ($mechanics as $mechanic)
                                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td class="px-6 py-4">
+                                        <td class="px-6 py-2">
                                             {{ $mechanic->id }}
                                         </td>
                                         <td scope="row"
-                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                             {{ $mechanic->name }}
                                         </td>
-                                        <td class="px-6 py-4">
+                                        <td class="px-6 py-2">
                                             {{ $mechanic->carOwner->name }}
                                         </td>
                                     </tr>
